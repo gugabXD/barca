f69d77f5c163b88c7d72e1c29cc0953398ade989
gugab xD <gustavobpasquali2@gmail.com>
2023-03-25 21:59:38 -0300

    classe barca

--- src/Barca.java
+++ src/Barca.java
@@ -1,42 +1,64 @@
-public class Barca {
-    public Barca(){
+package com.bcopstein;
+
+import java.util.regex.Pattern;
+
+public class BarcaBugs {
+    public static final int ASSENTOS_POR_FILA = 20;
+    public static final int FILAS = 60;
+    private boolean[][] assentos;
+    private int qtdadeAssentosOcupados;
+
+    public BarcaBugs(){
+        assentos = new boolean[FILAS][];
+        for(int i=0;i<FILAS;i++){
+            assentos[i] = new boolean[ASSENTOS_POR_FILA];
+            for(int j=0;j<ASSENTOS_POR_FILA;j++){
+                assentos[i][j] = false;
+            }
+        }
+        qtdadeAssentosOcupados = 0;
     }
 
-    //Método auxiliar projetado para facilitar testes
-    //Ocupa o lugar sem verificação
-    protected void ocupaLugar(int fila, int assento){
-
+    // Método auxiliar projetado para facilitar testes
+    protected void ocupaLugar(int fila,int assento){
+        assentos[fila][assento] = true;
+        qtdadeAssentosOcupados++;
     }
 
     /*
-       * retorna
-       *  0 – Identificador de assento inválido
-
-       · 1 – Assento ocupado
-
-       · 2 – Assento bloqueado devido a distribuição de peso
-
-       · 3 – Ok, assento atribuído ao passageiro.
-       * Exemplos: F02A12, F45A01, F33A18
-        */
-
+     * 0 – Identificador de assento inválido
+     * 1 – Assento ocupado
+     * 2 – Assento bloqueado devido a distribuição de peso
+     * 3 – Ok, assento atribuído ao passageiro.
+     */
     public int ocupaLugar(String assentoInformado){
-        if(verificaIdentificador(assentoInformado)==false) return 0;
-        return -1;
-    }
-
-    public boolean verificaIdentificador(String assentoInformado){
-        String[] assento = assentoInformado.split("", 0);
-        if(assento.length!=6) return false;
-        if(assento[0].equals("F")==false || assento[3].equals("A")==false) return false;
-        try {
-            int a = Integer.parseInt(assento[1] + assento[2]);
-            if (a <= 0 || a > 20) return false;
-            int f = Integer.parseInt(assento[4] + assento[5]);
-            if (f <= 0 || f > 60) return false;
-        } catch(Exception e){
-            return false;
+        // Verifica se é um assento valido
+        if (Pattern.matches("[F][0-8]{2}[A][0-8]{2}", assentoInformado) == false){
+            return 0;
         }
-        return true;
+        int fila = Integer.parseInt(assentoInformado.substring(1,3));
+        int assento = Integer.parseInt(assentoInformado.substring(4,6));
+        if (fila<0 || fila > 60){
+            return 0;
+        }
+        if (assento < 0 || assento >= 20){
+            return 0;
+        }
+        // Verifica se o assento já não está ocupado
+        if (assentos[fila][assento] == true){
+            return 1;
+        }
+        // Se tem até 100 passageiros, verifica se fila <= 20
+        if (qtdadeAssentosOcupados <= 100 && fila > 20){
+            return 1;
+        }
+        // Se tem mais de 100 e até de 200 passageiros, verifica se fila >= 40
+        if (qtdadeAssentosOcupados > 100 && qtdadeAssentosOcupados <= 200 && fila < 40){
+            return 2;
+        }
+        // Ocupa o assento
+        assentos[fila][assento] = true;
+        qtdadeAssentosOcupados++;
+        return 3;
     }
 }
