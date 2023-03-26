0576d7b319d44aebcbbd8b2572e89a04265cc6e3
gugab xD <gustavobpasquali2@gmail.com>
2023-03-25 21:15:36 -0300

    copiei bgl do professor

--- src/Barca.java
+++ src/Barca.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Barca {
+public class Barca {
+    public Barca(){
+    }
+
+    //Método auxiliar projetado para facilitar testes
+    //Ocupa o lugar sem verificação
+    protected void ocupaLugar(int fila, int assento){
+
+    }
+
+    /*
+       * retorna
+       *  0 – Identificador de assento inválido
+
+       · 1 – Assento ocupado
+
+       · 2 – Assento bloqueado devido a distribuição de peso
+
+       · 3 – Ok, assento atribuído ao passageiro.
+        */
+
+    public int ocupaLugar(String assentoInformado){
+        return -1;
+    }
 }
