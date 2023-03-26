d6624f3d4882d0a83e171a4beca3eea74f8874c7
gugab xD <gustavobpasquali2@gmail.com>
2023-03-25 21:35:48 -0300

    verificação do identificador de assento

--- src/Barca.java
+++ src/Barca.java
@@ -17,9 +17,17 @@ public class Barca {
        · 2 – Assento bloqueado devido a distribuição de peso
 
        · 3 – Ok, assento atribuído ao passageiro.
+       * Exemplos: F02A12, F45A01, F33A18
         */
 
     public int ocupaLugar(String assentoInformado){
+        String[] assento = assentoInformado.split("", 0);
+        if(assento.length!=6) return 0;
+        if(assento[0].equals("F")==false || assento[3].equals("A")==false) return 0;
+        int a = Integer.parseInt(assento[1]+assento[2]);
+        if(a<=0 || a>20) return 0;
+        int f = Integer.parseInt(assento[4]+assento[5]);
+        if(f<=0 || f>60) return 0;
         return -1;
     }
 }
